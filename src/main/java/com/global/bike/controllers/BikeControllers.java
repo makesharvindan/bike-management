9603eafabfa9d4a3b7c19b971e188ced8b3a2102
makesharvindan7@gmail.com <makesharvindan7@gmail.com>
2023-04-17 22:21:34 +0530

    changes for exception handling

--- src/main/java/com/global/bike/controllers/BikeControllers.java
+++ src/main/java/com/global/bike/controllers/BikeControllers.java
@@ -2,20 +2,15 @@ package com.global.bike.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.management.RuntimeErrorException;
 
 import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.global.bike.exception.RecordNotFoundException;
 import com.global.bike.models.Bike;
@@ -24,6 +19,7 @@ import com.global.bike.repo.BikeRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/bikes")
 @Slf4j
@@ -31,53 +27,55 @@ public class BikeControllers {
 
 	@Autowired
 	private BikeRepository bikeRepository;
-	
+
 	@GetMapping("/ping")
 	public String ping() {
 		return "OK";
 	}
-	
+
 	@GetMapping
-	public List<Bike> list(){
+	public List<Bike> list() {
 		List<Bike> bikes = new ArrayList<>();
 		return bikes;
 	}
-	
+
 	@PostMapping("/saveBike")
 	public String saveBikeDetails(@RequestBody Bike bike) {
 		System.out.println(bike);
 		Bike b = bikeRepository.save(bike);
 		return b.getId();
 	}
-	
+
 	@GetMapping("/{id}")
-	public Bike get(@PathVariable("id") String id) {
+	public Bike getByBikeId(@PathVariable("id") String id) {
 		try {
-			Bike b=  bikeRepository.findById(id);
-			if(b!=null)
+			Bike b = bikeRepository.findById(id);
+			if (b != null)
 				return b;
 			else
 				throw new RecordNotFoundException("no bike found");
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
 	}
-	
+
 	@GetMapping("/category")
-	public List<Bike> getByCategory(){
+	public List<Bike> getByCategory() {
 		try {
 			return bikeRepository.getBikesBySerialNumber();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
 	}
-	
+
 	@GetMapping("/group")
-	public List<BikeLists> getBikeListByGroupName(@RequestParam String groupName) throws RecordNotFoundException{
-		return bikeRepository.getBikesByGroupName(groupName);
+	public ResponseEntity<List<BikeLists>> getBikeListByGroupName(@RequestParam String groupName) throws Exception {
+		List<BikeLists> bikeLists = bikeRepository.getBikesByGroupName(groupName);
+			log.info("Bike List retrieved {}", bikeLists);
+//			return bikeLists.get();
+			return new ResponseEntity<>(bikeLists, HttpStatus.OK);
 	}
+
 }
